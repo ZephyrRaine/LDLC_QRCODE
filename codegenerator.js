f2e0b32e3384d13e0cc53eac75f96bc4c7d30b71
Colin <zephyr.raine@gmail.com>
2025-02-18 10:29:54 +0100

    save

--- codegenerator.js
+++ codegenerator.js
@@ -1,6 +1,7 @@
 const qrcode = require('qrcode');
 
 async function generateCode(data) {
+    
     const qrCodeImage = await qrcode.toDataURL(data, { width: 500, errorCorrectionLevel: 'H' });
     return qrCodeImage;
 }
--- generate.js
+++ generate.js
@@ -1,33 +1,44 @@
-
 var { generateCode } = require("./codegenerator");
 
-async function generatePDF()
+async function generatePDF(data, prefix, output_file, emoji)
 {
-var data = ["SELECT", "INSERT INTO", "UPDATE", "CONNECT", "DELETE", "JOIN", "WHERE", "AND", "OR"];
 console.log(data);
 var codes = await Promise.all(data.map(async (d) => {
     return {
         label: d,
-        src: `${await generateCode("<SQL_KEYWORD,"+d+">")}`
+        src: `${await generateCode(`<${prefix},${d}>`)}`
     };
 }));
 
 console.log(codes);
 //GENERATE HTML FILE WITH QR CODES NEXT TO LABELS
 var fs = require('fs');
-var html = '<!DOCTYPE html><html><head><title>QR Codes</title></head><body>';
+var html = '<!DOCTYPE html><html><head><title>QR Codes</title>';
+html += '<link rel="preconnect" href="https://fonts.googleapis.com">';
+html += '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+html += '<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Sono:wght@200..800&display=swap" rel="stylesheet">';
+html += '</head><body>';
 //ADD STYLE.CSS TO HTML FILE
 html += '<link rel="stylesheet" type="text/css" href="style.css">';
 html += '<div class="container">';
 codes.forEach((c) => {
     html += `<div class="parent">`;
     html += `<img src="${c.src}" alt="${c.label}"/><p>${c.label}</p>`;
+    html += `<span class="output-file">${emoji}</span>`;
     html += `</div>`;
 });
 html += '</div>';
 html += '</body></html>';
-fs.writeFileSync('qrcodes.html', html);
+fs.writeFileSync(output_file+".html", html);
 
 }
 
-generatePDF();
+// var data = ["SELECT", "INSERT INTO", "UPDATE", "CONNECT", "DELETE", "JOIN", "WHERE", "AND", "OR"];
+// generatePDF(data, "SQL_KEYWORD", "qrcodes");
+
+//PARSE data.json and generate QR codes
+var fs = require('fs');
+var data = JSON.parse(fs.readFileSync('data.json'));
+for (var key in data) {
+    generatePDF(data[key]["data"], "VALUE", `${key}`, data[key]["emoji"]);
+}
